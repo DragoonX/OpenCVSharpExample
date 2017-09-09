d2b62e4df49cc70896628a342881799a09752f88
DragoonX <subzero.16@hotmail.com>
2017-09-09 04:19:04 +0300

    sum
    descr.

---
+++ OpenCVSharp1/OpenCVSharp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace OpenCVSharp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(460, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(274, 287);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Grayscale",
+            "Reverse",
+            "Logarithm",
+            "Gama"});
+            this.listBox1.Location = new System.Drawing.Point(292, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(162, 148);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Resim Yükle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Uygula";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(540, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Resmi Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 415);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ OpenCVSharp1/OpenCVSharp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace OpenCVSharp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        Mat src = new Mat();
+        Mat dstrev = new Mat();
+        private Mat dstlog;
+        private Mat dstgama;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG |All files (*.*)|*.*";
+            dialog.InitialDirectory = @"D:\OCV\";
+            dialog.Title = "Please select an image file.";
+
+            if (dialog.ShowDialog() == DialogResult.OK){
+                pictureBox1.ImageLocation = dialog.FileName;
+            }
+            else
+                return;
+
+            src = Cv2.ImRead(dialog.FileName, LoadMode.GrayScale);
+            dstrev = src.EmptyClone();
+            dstlog = src.EmptyClone();
+            dstgama = src.EmptyClone();
+
+        }//button1_click end.
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.GetSelected(0)){
+                pictureBox2.Image = BitmapConverter.ToBitmap(src);
+            }
+            if (listBox1.GetSelected(1)){
+                for (int y=0;y<src.Rows;y++){
+                    for (int x =0;x<src.Cols;x++){
+                        byte sum = src.Get<byte>(y, x);
+                        dstrev.Set<byte>(y, x, (byte)(255 - sum));
+                    }
+                }
+                pictureBox2.Image = BitmapConverter.ToBitmap(dstrev);
+            }
+            if (listBox1.GetSelected(2)){
+                for (int y = 0; y < src.Rows; y++){
+                    for (int x = 0; x < src.Cols; x++){
+                        byte sum = src.Get<byte>(y, x);
+                        dstlog.Set<byte>(y, x, (byte)(75 * Math.Log10(1.0 + (double)sum)));
+                    }
+                }
+                pictureBox2.Image = BitmapConverter.ToBitmap(dstlog);
+            }
+            if (listBox1.GetSelected(3)){
+                for (int y = 0; y < src.Rows; y++){
+                    for (int x = 0; x < src.Cols; x++){
+                        byte sum = src.Get<byte>(y, x);
+                        dstgama.Set<byte>(y, x, (byte)(20.0 * Math.Pow((double)sum,4.0)));
+                    }
+                }
+                pictureBox2.Image = BitmapConverter.ToBitmap(dstgama);
+            }
+        }
+    }//partial class end.
+}//namespace end.
